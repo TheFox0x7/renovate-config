2e85213c96918da190a31bb9802979fb73a29f6f
TheFox0x7 <thefox0x7@gmail.com>
2025-12-19 20:01:29 +0100

    test out github port

--- config.js
+++ config.js
@@ -1,24 +1,27 @@
 module.exports = {
-    "endpoint": "https://gitea.com/api/v1",
-    "gitAuthor": "Renovate Bot <renovate-bot@gitea.com>",
-    "platform": "gitea",
-    "onboardingConfigFileName": "renovate.json5",
-    "autodiscover": true,
-    "autodiscoverFilter": ["gitea/*"],
-    "optimizeForDisabled": true,
-    "forkProcessing": "disabled",
-    "dryRun": null,
-    "binarySource": "install",
-    "hostRules": [
-        {
-            "matchHost": "docker.io",
-            "username": process.env.HUB_DOCKER_COM_USER,
-            "password": process.env.HUB_DOCKER_COM_TOKEN
-        }
-    ],
-    "allowedPostUpgradeCommands": [
-        // [gitea/helm-chart] Update README.md on dependency changes in values.yaml
-        "install-tool node",
-        "make readme"
-    ]
+  // "endpoint": "https://gitea.com/api/v1",
+  "gitAuthor": "Renovate Bot <renovate-bot@gitea.com>",
+  // "platform": "gitea",
+  "onboardingConfigFileName": "renovate.json5",
+  "repositories": [
+    "gitea"
+  ],
+  // "autodiscover": true,
+  // "autodiscoverFilter": ["gitea/*"],
+  "optimizeForDisabled": true,
+  "forkProcessing": "disabled",
+  "dryRun": null,
+  "binarySource": "install",
+  "hostRules": [
+    {
+      "matchHost": "docker.io",
+      "username": process.env.HUB_DOCKER_COM_USER,
+      "password": process.env.HUB_DOCKER_COM_TOKEN
+    }
+  ],
+  // "allowedPostUpgradeCommands": [
+  //   // [gitea/helm-chart] Update README.md on dependency changes in values.yaml
+  //   "install-tool node",
+  //   "make readme"
+  // ]
 };
